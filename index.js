5b0beb348bbd3f947b70c9a724b247a7c0add806
bartek8b <163062773+bartek8b@users.noreply.github.com>
2024-08-16 15:33:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-function showSidebar(){
-    const sidebar = document.querySelector('#sideBar');
-    const menuButton = document.querySelector('#menuButton');
-    const XButton = document.querySelector('#XButton');
+const sidebar = document.querySelector('#sideBar');
+const menuButton = document.querySelector('#menuButton');
+const XButton = document.querySelector('#xButton');
 
+function showSidebar(){
     sidebar.style.display ='flex';
     menuButton.style.display = 'none';
     XButton.style.display ='flex';
 }
 function hideSidebar(){
-    const sidebar = document.querySelector('#sideBar');
-    const menuButton = document.querySelector('#menuButton');
-    const XButton = document.querySelector('#XButton');
-
     sidebar.style.display ='none';
     menuButton.style.display = 'flex';
     XButton.style.display ='none';
 }
+
+function updateWidth(){
+    const windowWidth = window.innerWidth;
+
+    if(windowWidth >= 767){
+        sidebar.style.display ='none';
+        menuButton.style.display = 'none';
+        XButton.style.display ='none';
+    }
+    else{
+        sidebar.style.display ='none';
+        menuButton.style.display = 'flex';
+        XButton.style.display ='none';
+    }
+}
+
+window.addEventListener("resize", updateWidth);
