23561c4df7527abe27deebccca4f37fd4a1edbdd
bartek8b <163062773+bartek8b@users.noreply.github.com>
2024-10-18 23:56:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,159 @@
-const sidebar = document.querySelector('#sideBar');
-const menuButton = document.querySelector('#menuButton');
-const xButton = document.querySelector('#xButton');
+// ---------------------MENU--------------------
 
-function showSidebar(){
-    sidebar.style.display ='flex';
-    menuButton.style.display = 'none';
-    xButton.style.display ='flex';
+function showSidebar() {
+	document.getElementById("sideBar").style.display = "flex";
+	document.getElementById("menuButton").style.display = "none";
+	document.getElementById("xButton").style.display = "flex";
 
-    sidebar.animate([
-        {transform: 'translateX(100%)'},
-        {transform: 'translateX(0)'}],
-        {duration: 500, fill: 'forwards'});
+	document
+		.getElementById("sideBar")
+		.animate(
+			[{ transform: "translateX(100%)" }, { transform: "translateX(0)" }],
+			{ duration: 500, fill: "forwards" }
+		);
 }
 
-function hideSidebar(){
-    menuButton.style.display = 'flex';
-    xButton.style.display ='none';
+function hideSidebar() {
+	document.getElementById("menuButton").style.display = "flex";
+	document.getElementById("xButton").style.display = "none";
 
-    sidebar.animate([
-        {transform: 'translateX(0)'},
-        {transform: 'translateX(100%)'}],
-        {duration: 500, fill: 'forwards'});
-    
-    updateWidth();
+	document
+		.getElementById("sideBar")
+		.animate(
+			[{ transform: "translateX(0)" }, { transform: "translateX(100%)" }],
+			{ duration: 500, fill: "forwards" }
+		);
+
+	updateWidth();
 }
 
-function updateWidth(){
-    const windowWidth = window.innerWidth;
+function updateWidth() {
+	const windowWidth = window.innerWidth;
 
-    if(windowWidth > 768){
-        sidebar.style.display ='none';
-        menuButton.style.display = 'none';
-        xButton.style.display ='none';
-    }
-    else{
-        menuButton.style.display = 'flex';
-        xButton.style.display ='none';
-    }
+	if (windowWidth > 768) {
+		document.getElementById("sideBar").style.display = "none";
+		document.getElementById("menuButton").style.display = "none";
+		document.getElementById("xButton").style.display = "none";
+	} else {
+		document.getElementById("menuButton").style.display = "flex";
+		document.getElementById("xButton").style.display = "none";
+	}
 }
 
 window.addEventListener("resize", updateWidth);
 
+// ---------------------GALLERY ALBUMS--------------------
 
+const diplomasArray = [
+	"images/gallery/diplomas/1.jpg",
+	"images/gallery/diplomas/2.jpg",
+	"images/gallery/diplomas/3.jpg",
+	"images/gallery/diplomas/4.jpg",
+	"images/gallery/diplomas/5.jpg",
+	"images/gallery/diplomas/6.jpg",
+	"images/gallery/diplomas/7.jpg",
+	"images/gallery/diplomas/8.jpg",
+	"images/gallery/diplomas/9.jpg",
+	"images/gallery/diplomas/10.jpg",
+	"images/gallery/diplomas/11.jpg",
+	"images/gallery/diplomas/12.jpg",
+	"images/gallery/diplomas/13.jpg",
+	"images/gallery/diplomas/14.jpg",
+	"images/gallery/diplomas/15.jpg",
+	"images/gallery/diplomas/16.jpg",
+	"images/gallery/diplomas/17.jpg",
+	"images/gallery/diplomas/18.jpg",
+	"images/gallery/diplomas/19.jpg",
+	"images/gallery/diplomas/20.jpg",
+	"images/gallery/diplomas/21.jpg",
+	"images/gallery/diplomas/22.jpg",
+	"images/gallery/diplomas/23.jpg",
+	"images/gallery/diplomas/24.jpg",
+	"images/gallery/diplomas/25.jpg",
+];
 
+const gymArray = [
+	"images/gallery/gym/1.jpg",
+	"images/gallery/gym/2.jpg",
+	"images/gallery/gym/3.jpg",
+];
 
+const trainingArray = [
+	"images/gallery/training/1.jpg",
+	"images/gallery/training/2.jpg",
+	"images/gallery/training/3.jpg",
+	"images/gallery/training/4.jpg",
+];
 
+function getRandomImage(arrayName) {
+	const randomIndex = Math.floor(Math.random() * arrayName.length);
+	return arrayName[randomIndex];
+}
+
+function displayRandomImage(albumCover, arrayName) {
+	const img = document.createElement("img");
+	img.src = getRandomImage(arrayName);
+	document.getElementById(albumCover).appendChild(img);
+}
+
+displayRandomImage("diplomasCover", diplomasArray);
+displayRandomImage("gymCover", gymArray);
+displayRandomImage("trainingCover", trainingArray);
+
+// ---------------------DISPLAY PHOTO--------------------
+
+let photoCounter = 0;
+let covers = document.querySelectorAll(".albumCover");
+
+function openAlbum(arrayName, albumDisplay, albumPictureDisplay) {
+	let counters = document.querySelectorAll(".photoCounter");
+
+	counters.forEach(counter => {
+		counter.innerHTML = photoCounter + 1 + "/" + arrayName.length;
+	});
+
+	covers.forEach(cover => {
+		cover.style.display = "none";
+	});
+
+	document.getElementById(albumDisplay).style.display = "block";
+	document.getElementById(albumPictureDisplay).src = arrayName[0];
+}
+
+function closeAlbum(albumDisplay) {
+	covers.forEach(cover => {
+		cover.style.display = "block";
+	});
+	document.getElementById(albumDisplay).style.display = "none";
+	photoCounter = 0;
+}
+
+function nextPhoto(arrayName, albumPictureDisplay) {
+	if (photoCounter == arrayName.length - 1) {
+		photoCounter = 0;
+	} else {
+		photoCounter++;
+	}
+
+	let counters = document.querySelectorAll(".photoCounter");
+	counters.forEach(counter => {
+		counter.innerHTML = photoCounter + 1 + "/" + arrayName.length;
+	});
+
+	document.getElementById(albumPictureDisplay).src = arrayName[photoCounter];
+}
+
+function prevPhoto(arrayName, albumPictureDisplay) {
+	if (photoCounter == 0) {
+		photoCounter = arrayName.length - 1;
+	} else {
+		photoCounter--;
+	}
+
+	let counters = document.querySelectorAll(".photoCounter");
+	counters.forEach(counter => {
+		counter.innerHTML = photoCounter + 1 + "/" + arrayName.length;
+	});
+
+	document.getElementById(albumPictureDisplay).src = arrayName[photoCounter];
+}
