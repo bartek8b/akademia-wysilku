51a4c22bbb7eea31af6c32e8f3a7e22facaecd27
bartek8b <163062773+bartek8b@users.noreply.github.com>
2024-11-24 21:24:56 +0100

    Add files via upload

--- oferta.js
+++ oferta.js
@@ -3,17 +3,21 @@ containers.forEach(container => {
 	container.addEventListener("mouseover", () => {
 		const child1 = container.querySelector(".offerTable");
         const child2 = container.querySelector(".offerClausure");
-        const child3 = container.querySelector(".offerCaption");
+        const child3 = container.querySelector(".offerDetails");
+        const child4 = container.querySelector(".iconOffer");
 		child1.style.display = "block";
         child2.style.display = "block";
         child3.style.display = "none";
+        child4.style.fontSize = "2rem";
 	});
 	container.addEventListener("mouseout", () => {
 		const child1 = container.querySelector(".offerTable");
         const child2 = container.querySelector(".offerClausure");
-        const child3 = container.querySelector(".offerCaption");
+        const child3 = container.querySelector(".offerDetails");
+        const child4 = container.querySelector(".iconOffer");
 		child1.style.display = "none";
         child2.style.display = "none";
         child3.style.display = "block";
+        child4.style.fontSize = "4rem";
 	});
 });
